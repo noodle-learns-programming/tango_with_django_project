d6e2081731069c778e91c1e7fa588f42445f4bb3
noodle-learns-programming <noodle.learns.programming@gmail.com>
2015-01-07 01:08:04 +0700

    templatetags + text_bold with syntax: load bold_text form rango_extra

--- rango/templatetags/rango_extras.py
+++ rango/templatetags/rango_extras.py
@@ -1,13 +1,6 @@
 from django import template
-from rango.models import Category
-
 register = template.Library()
-
-@register.inclusion_tag('rango/cats.html')
-def get_category_list():
-    return {'cats': Category.objects.all()}
-
 
 @register.simple_tag
 def bold_text(text):
-    return '<b>' + text + '</b>'
+    return '<b style="color:blue;">' + text + '</b>'
